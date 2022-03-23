4c4b294011429b60bcad9f44553bf624a3c46b99
CookieKastanie <jerem0205@yahoo.fr>
2022-03-23 08:33:55 +0100

    ...

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
 const instanceUrl = 'https://invidious.fdn.fr';
 
-const parseVideoId = url => {
-    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-    const match = url.match(regExp);
-    return (match && match[7].length === 11) ? match[7] : false;
-}
-
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         id: 'open_in_invidious',
@@ -15,9 +9,9 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+chrome.contextMenus.onClicked.addListener(async info => {
     if(info.menuItemId === 'open_in_invidious' && info.linkUrl) {
-        const id = parseVideoId(info.linkUrl);
-        if(id) await chrome.tabs.create({url: `${instanceUrl}/watch?v=${id}`});
+        const url = new URL(info.linkUrl);
+        await chrome.tabs.create({url: `${instanceUrl}${url.pathname}${url.search}`});
     }
 });
